942f134217b4bde90d756af28c04fcf38a6500b8
devanshishah27 <devanshishah27@yahoo.in>
2015-05-08 19:36:19 -0700

    MyFlicker
    Version1

---
+++ app/src/main/java/com/example/devanshi/myflicker/Constants.java
@@ -0,0 +1,22 @@
+package com.example.devanshi.myflicker;
+
+/**
+ * Created by DEVANSHI on 08-05-2015.
+ */
+public class Constants {
+
+
+        public static final int NUM_PHOTOS = 10;
+        public static final String TAG = "my code";
+        public static final String API_KEY = "a36b459e17d0af49f20bc7d523aab0c1";
+        public static final String SECRET = "1a31bd72bbfb0289";
+        //    public static final String LATITUDE = "34.0672280";
+//    public static final String LONGITUDE = "-118.1667410";
+        public static final String RADIUS = "1";
+
+        public static final String LATITUDE = "40.7127837";
+        public static final String LONGITUDE = "-74.00594130000002";
+
+
+
+}
